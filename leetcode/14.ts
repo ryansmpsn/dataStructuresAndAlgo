246ebd875bf443f2f8a781bb5f377e899dcfc936
Ryan Simpson <ryzizn@gmail.com>
2025-01-30 15:48:59 -0500

    lc: 14 Longest Common Prefix

---
+++ leetcode/14.ts
@@ -0,0 +1,17 @@
+function longestCommonPrefix(strs: string[]): string {
+  let output = '';
+
+  for (let i: number = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
+
+    const isCommonPrefix: boolean = strs.every((str) => char === str[i]);
+
+    if (isCommonPrefix) {
+      output += char;
+    } else {
+      return output;
+    }
+  }
+
+  return output;
+}
